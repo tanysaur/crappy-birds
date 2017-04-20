a8a2ed2eb83628dea224df5cc4b2923108b2ce36
Ysa Tan <tanysaur@gmail.com>
2017-04-19 22:41:07 -0500

    Working Leap: swipe + keyTap

--- games/crappyBirdz.js
+++ games/crappyBirdz.js
@@ -6,62 +6,94 @@ var scoreP2 = 0;
 var winner;
 var currentTurn;
 var turns = 16;
+var x = 0, y = 0;
 
 //===== Leap motion ======
 
-  
-$(document).ready(function() {
-      window.controller = new Leap.Controller({enableGestures: true});
 
-      var swiper = controller.gesture('swipe');
-    // var tapper = controller.gesture('screenTap');
+$(document).ready(function () {
 
-      var tolerance = 50;
-      var cooloff = 100;
-
-      var x = 1, y = 1;
-
-      var updateHighlight = function() {
+    
+    var frameLen = 0;
+    var highlightedTile;
+    function updateHighlight(a, b) {
+        console.log(a, b)
         $('.grid div').removeClass('highlight');
-        $('.grid #d'+ x +"_" + y).addClass('highlight');
-      }
-
-      var slider = _.debounce(function(xDir, yDir) {
-        x += xDir;
-        x = (x + 4) % 4;
-        y += yDir;
-        y = (y + 4) % 4;
-        console.log("x:" + x);
-        console.log("y:" + y);
-
-        console.log('.grid #d'+ x + "_" + y);
-        updateHighlight();
-      }, cooloff);
-
-      swiper.update(function(g) {
-        if (Math.abs(g.translation()[0]) > tolerance || Math.abs(g.translation()[1]) > tolerance) {
-          var xDir = Math.abs(g.translation()[0]) > tolerance ? (g.translation()[0] > 0 ? -1 : 1) : 0;
-          var yDir = Math.abs(g.translation()[1]) > tolerance ? (g.translation()[1] < 0 ? -1 : 1) : 0;
-          slider(xDir, yDir);
-        }
+        $('.grid #d' + a + "_" + b).addClass('highlight');
+        highlightedTile = $('#d' + a + "_" + b);
         
-      });
+    }
+    updateHighlight(0, 0);
 
-      controller.connect();
-      updateHighlight();
+        var prev = 0;
+    var controller = Leap.loop({ enableGestures: true }, function (frame) {
+        
+        //console.log(frame.gestures.length)
+        frameLen++;
+        //console.log(frameLen)
 
-     
-// ====== NEED TO ADD screenTap listener functionality
-// https://developer.leapmotion.com/documentation/javascript/api/Leap.ScreenTapGesture.html
+        for (var i = 0; i < frame.gestures.length; i++) {
+            var gesture = frame.gestures[i];
+            switch (gesture.type) {
+                case "keyTap":
+                    console.log("Key Tap Gesture");
+                    highlightedTile.click();
+                    console.log(highlightedTile)
+                    break;
+                // case "screenTap":
+                //     console.log("Screen Tap Gesture");
+                //     break;
+                case "swipe":
+                    console.log("Swipe Gesture");
+                    var swipedAt = new Date().getTime();
+                    console.log(prev)
+                    console.log(swipedAt);
+                    if ((swipedAt - prev) > 500) {
+                        var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+                        //Classify as right-left or up-down
+                        if (isHorizontal) {
+                            if (gesture.direction[0] > 0) {
+                                swipeDirection = "right";
+                                x++;
+                            } else {
+                                swipeDirection = "left";
+                                x--;
+                            }
+                        } else { //vertical
+                            if (gesture.direction[1] > 0) {
+                                swipeDirection = "up";
+                                y--;
+                            } else {
+                                swipeDirection = "down";
+                                y++;
+                            }
+                        }
+                        if (x < 0)
+                            x = 0;
+                        if (x > 3)
+                            x = 3;
 
-// var controller = Leap.loop({enableGestures: true}, function(frame){
-//   //... handle frame data
-// });
+                        if (y < 0)
+                            y = 0;
+                        if (y > 3)
+                            y = 3;
 
-// controller.on("gesture", function(gesture){
-//   //... handle gesture object
-//  console.log("tap tap tap!");
-// });
+                        //console.log(swipeDirection)
+
+                        updateHighlight(x, y)
+
+
+
+                    }
+                    if((swipedAt - prev) > 500 )
+                        prev = swipedAt;
+                    break;
+            }
+        };
+        frameLen = 0;
+
+    });
+
 
 
 });//docReady
@@ -70,108 +102,114 @@ $(document).ready(function() {
 
 //====== Game Play ====== 
 
-$(document).ready(function(){
+$(document).ready(function () {
     resetBoard();
 }); // docReady
 
 
 // Game play
-$(document).keyup(function(event){ // keypress? what is the leapmotion equivalent?
+$(document).keyup(function (event) { // keypress? what is the leapmotion equivalent?
 
     //Player presses spacebar to start the game
-    if (event.keyCode == 32){
+    if (event.keyCode == 32) {
         console.log("Game starts!");
     }
 
     initGame();
     resetBoard();
 
-}); 
-    
-    //initializes game stats  
-    function initGame(){
-        p1 = 0;
-        p2 = 0;
+});
 
-    }//initGame
 
-    function selectTile(){
-        var itemsArray = [{
-            picture: "../public/images/car-poop.jpeg",
-            score: 1000
-        },{
-            picture: "../public/images/bird_2.jpeg",
-            score: 500
-        },{
-            picture: "../public/images/bird.png",
-            score: 200    
-        }];
 
-        var randomItem = Math.floor((Math.random() * itemsArray.length) +1);
+//initializes game stats  
+function initGame() {
+    p1 = 0;
+    p2 = 0;
 
-        console.log(itemsArray[randomItem].picture + " " + itemsArray[randomItem].score);
+}//initGame
 
-        return itemsArray[randomItem];
+function selectTile() {
+    var itemsArray = [{
+        picture: "../public/images/car-poop.jpeg",
+        score: 1000
+    }, {
+        picture: "../public/images/bird_2.jpeg",
+        score: 500
+    }, {
+        picture: "../public/images/bird.png",
+        score: 200
+    }];
+
+    var randomItem = Math.floor((Math.random() * itemsArray.length) + 1);
+
+    console.log(itemsArray[randomItem].picture + " " + itemsArray[randomItem].score);
+
+    return itemsArray[randomItem];
+}
+
+//resets board with random pictures
+function resetBoard() {
+
+    // 4x4 2-dimensional array
+    for (var row = 0; row < 4; row++) {
+        for (var col = 0; col < 4; col++) {
+            var item = selectTile();
+
+            //insert img to DOM, data-attr: picture value
+            $('#d' + row + '_' + col)
+                .data('score', item.score)
+                .css("background-image", item.picture);
+        }
     }
+}//resetBoard
 
-    //resets board with random pictures
-    function resetBoard(){
-       
-        // 4x4 2-dimensional array
-        for (var row = 0; row < 4; row++){
-            for (var col = 0; col < 4; col++){
-                var item = selectTile();     
+function gameStarts() {
+    while (turns != 0) {
+        //p1 starts
+        //player chooses cell, if cell is used, show error ======= leapmotion: rock
 
-                //insert img to DOM, data-attr: picture value
-                $('#d' + row + '_' + col)
-                    .data('score', item.score)
-                    .css("background-image", item.picture);        
-            }
-        }
-    }//resetBoard
+        //unblur picture
+        $("#d" + "0_0").addClass("birdOff");
+        //show picture_score
 
-    function gameStarts(){
-        while ( turns != 0){
-            //p1 starts
-                //player chooses cell, if cell is used, show error ======= leapmotion: rock
-                
-                //unblur picture
-                $("#d" + "0_0").addClass("birdOff"); 
-                //show picture_score
+        scoreP1 += picture_score;
 
-                scoreP1 += picture_score; 
+        //mark cell as used using its data attr, class="used", we can also use backend to store the score & or picture
 
-                //mark cell as used using its data attr, class="used", we can also use backend to store the score & or picture
+        //decrement turns
+        turns--;
 
-                //decrement turns
-                turns--;
-            
-            //p2 or computer picks random cell
-                //computer chooses unused cell, 
+        //p2 or computer picks random cell
+        //computer chooses unused cell, 
 
-                //unblur picture of chosen cell
-                $("#d" + "0_0").addClass("birdOff");
+        //unblur picture of chosen cell
+        $("#d" + "0_0").addClass("birdOff");
 
-                scoreP2 += picture_score;
+        scoreP2 += picture_score;
 
-                //marks cell as used using its data attr, class="used";
+        //marks cell as used using its data attr, class="used";
 
-                //decrement turns
-                turns--;
-        }
-        declareWinner();
+        //decrement turns
+        turns--;
+    }
+    declareWinner();
 
-    }//gameStarts
+}//gameStarts
 
-    function declareWinner(){
-        if (scoreP1 > scoreP2){
-            console.log("Player1 wins!");
-            $("#game-status").text("Player 1 wins the game!");
-        } else if (scoreP1 < scoreP2){
-            console.log("Player 2 wins!");
-            $("#game-status").text("Player 2 wins the game!");
-        } else {
-            console.log("It's a tie!");
-            $("#game-status").text("It's a tie!'");        
-        }
-    }//declare winner
+$('#d' + x + '_' + y).on('click', function(){
+    console.log('this');
+});
+
+function declareWinner() {
+    if (scoreP1 > scoreP2) {
+        console.log("Player1 wins!");
+        $("#game-status").text("Player 1 wins the game!");
+    } else if (scoreP1 < scoreP2) {
+        console.log("Player 2 wins!");
+        $("#game-status").text("Player 2 wins the game!");
+    } else {
+        console.log("It's a tie!");
+        $("#game-status").text("It's a tie!'");
+    }
+}//declare winner
